cb16c2eb451d20f3e496eb7c6f82df908013190e
denisenricohasyim93 <denisenricohasyim93@gmail.com>
2018-12-10 17:14:47 +0700

    another commit

--- src/v0/services/forgetPassword.js
+++ src/v0/services/forgetPassword.js
@@ -5,80 +5,81 @@ const fetch = require('node-fetch')
 var { ERPNEXT_API_BASE_URL, jsonwebtokensecret, emailAccount, erpNextCredentials } = require('../credentials')
 
 router.get('/', function (req, res) {
-    res.send({status : 200, message : 'Welcome to DB User Management Service - v0 - Forget Password API '})
+    res.send({ status: 200, message: 'Welcome to DB User Management Service - v0 - Forget Password API ' })
 })
 
-router.post('/getTokenFirst', async function (req, res) {
+router.post('/getTokenFirst', function (req, res) {
     var ketemu = false
     var itemKetemu = null
-    await fetch(ERPNEXT_API_BASE_URL+'/api/resource/User', {
+    fetch(ERPNEXT_API_BASE_URL + '/api/resource/User', {
         method: 'GET',
         headers: {
-            'Authorization': "token "+erpNextCredentials.api_key+":"+erpNextCredentials.api_secret
+            'Authorization': "token " + erpNextCredentials.api_key + ":" + erpNextCredentials.api_secret
         },
     })
-    .then(response => {
-        response.json()
-        .then(json => {
-            if (json.data) {
-                json.data.map((item, index) => {
-                    if (req.body.email === item.email) {
-                        itemKetemu = item
-                        ketemu = true
+        .then(response => {
+            response.json()
+                .then(xyz => {
+                    if (xyz.data) {
+                        xyz.data.map((item, index) => {
+                            if (req.body.email === item.name) {
+                                itemKetemu = item
+                                ketemu = true
+                            }
+                        })
+                        if (ketemu) {
+                            // E-MAIL KE YANG PUNYA AKUN DAN BERITAHU TOKEN
+                            var nodemailer = require('nodemailer');
+                            var transporter = nodemailer.createTransport({
+                                service: 'gmail',
+                                auth: {
+                                    user: emailAccount.user,
+                                    pass: emailAccount.pass
+                                }
+                            });
+                            var token = jwt.sign({}, jsonwebtokensecret);
+                            const mailOptions = {
+                                from: emailAccount.user, // sender address
+                                to: req.body.email, // list of receivers
+                                subject: 'SMARTFARM - Pemberitahuan Password', // Subject line
+                                html: '<p>Copy Token Ini Ke App : ' + token + '</p>'// plain text body
+                            };
+                            transporter.sendMail(mailOptions, function (err, info) {
+                                if (err) {
+                                    res.send({
+                                        status: 503,
+                                        message: 'Gagal Mengirim Token via E-Mail',
+                                        err: err
+                                    })
+                                } else {
+                                    res.send({
+                                        status: 200,
+                                        message: 'Sukses Mengirim Token Ke ' + req.body.email
+                                    })
+                                }
+                            });
+                        } else {
+                            // BERITAHU REQUESTER BAHWA AKUN TIDAK ADA
+                            res.send({
+                                status: 504,
+                                message: 'E-Mail Tidak Terdaftar di Sistem'
+                            })
+                        }
+                    } else {
+                        res.send({ status: 500, message: 'list kosong ...' })
                     }
+                    // res.send({status : 200, hasil : xyz.data})
                 })
-            } else {
-                res.send({status : 500, message : 'list kosong ...'})
-            }
+                .catch((err) => {
+                    res.send({ status: 501, message: err })
+                })
         })
         .catch((err) => {
-            res.send({status : 501, message : err})
+            res.send({
+                status: 502,
+                message: err
+            })
         })
-    })
-    .catch((err) => {
-        res.send({
-            status : 502,
-            message : err
-        })
-    })
-    if (ketemu) {
-        // E-MAIL KE YANG PUNYA AKUN DAN BERITAHU TOKEN
-        var nodemailer = require('nodemailer');
-        var transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: emailAccount.user,
-                pass: emailAccount.pass
-            }
-        });
-        var token = jwt.sign({}, jsonwebtokensecret);
-        const mailOptions = {
-            from: emailAccount.user, // sender address
-            to: req.body.email, // list of receivers
-            subject: 'SMARTFARM - Pemberitahuan Password', // Subject line
-            html: '<p>Copy Token Ini Ke App : ' + token + '</p>'// plain text body
-        };
-        transporter.sendMail(mailOptions, function (err, info) {
-            if (err) {
-                res.send({
-                    status: 503,
-                    message: 'Gagal Mengirim Token via E-Mail',
-                    err: err
-                })
-            } else {
-                res.send({
-                    status: 200,
-                    message: 'Sukses Mengirim Token Ke ' + req.body.email
-                })
-            }
-        });
-    } else {
-        // BERITAHU REQUESTER BAHWA AKUN TIDAK ADA
-        res.send({
-            status: 504,
-            message: 'E-Mail Tidak Terdaftar di Sistem'
-        })
-    }
 })
 
 module.exports = router
