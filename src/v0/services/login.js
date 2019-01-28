3897c5e3bf76dd5db1d04a095cb1f151c725415e
denisenricohasyim93 <denisenricohasyim93@gmail.com>
2019-01-28 14:54:59 +0700

    another commit

--- src/v0/services/login.js
+++ src/v0/services/login.js
@@ -24,15 +24,15 @@ router.post('/', function (req, res) {
                 .then(json => {
                     if (json.message === "Logged In") {
                         var token = jwt.sign({}, jsonwebtokensecret);
-                        res.send({ status: 200, message: 'Login Berhasil', token: token, json : json})
+                        res.send({ status: 200, message: 'Login Berhasil', token: token, json: json })
                     } else {
-                        res.send({ status: 500, message: 'Login Gagal'})
+                        res.send({ status: 500, message: 'Login Gagal' })
                     }
                 })
                 .catch((err) => {
-                    res.send({ 
-                        status: 501, 
-                        message: 'Login Gagal' 
+                    res.send({
+                        status: 501,
+                        message: 'Login Gagal'
                         // err : err 
                     })
                 })
@@ -47,43 +47,74 @@ router.post('/', function (req, res) {
 })
 
 router.post('/forOwner', function (req, res) {
-    fetch(ERPNEXT_API_BASE_URL + '/api/resource/Supplier?fields=["name","owner_web_app_pass"]', {
-        method: 'get',
+    fetch(ERPNEXT_API_BASE_URL + '/api/method/login', {
+        method: 'POST',
         headers: {
-            'Authorization': 'token ' + erpNextCredentials.api_key + ':' + erpNextCredentials.api_secret
-        }
-    }).then(async(response) => {
-        await response.json()
-            .then((async json => {
-                    if (json.data) {
-                            // res.send({ status: 200, message: 'get crop list succeed', json: json })
-                            let ketemu = null;
-                            await json.data.map(async (item, index) => {
-                                if (item.name === req.body.name) {
-                                    ketemu = await item
-                                }
-                            })
-                            if (ketemu) {
-                                if (ketemu.owner_web_app_pass === req.body.password) {
-                                    var token = jwt.sign({}, jsonwebtokensecret);
-                                    res.send({status: 200, message: 'Selamat, Login Berhasil', token: token})
-                                } else {
-                                    res.send({status: 500, message: 'Maaf, Login Gagal, Password Salah'})
-                                }
-                            } else {
-                                res.send({status: 500, message: 'Maaf, Login Gagal, Nama Owner tidak ada di database'})
-                            }
-                    } else {
-                        res.send({ status: 501, message: 'get owner list failed', json: json })
-                    }
-                }
-            ))
-            .catch((err) => {
-                res.send({ status: 502, message: 'get owner list failed', err: err })
-            })
-    }).catch((err) => {
-        res.send({ status: 503, message: 'get owner list failed', err: err })
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify({
+            "usr": req.body.email,
+            "pwd": req.body.password
+        })
     })
+        .then(response => {
+            response.json()
+                .then(json => {
+                    if (json.message === "Logged In") {
+                        // var token = jwt.sign({}, jsonwebtokensecret);
+                        // res.send({ status: 200, message: 'Login Berhasil', token: token, json : json})
+                        fetch(ERPNEXT_API_BASE_URL + '/api/resource/Supplier?fields=["name","owner_web_app_pass"]', {
+                            method: 'get',
+                            headers: {
+                                'Authorization': 'token ' + erpNextCredentials.api_key + ':' + erpNextCredentials.api_secret
+                            }
+                        }).then(async (response) => {
+                            await response.json()
+                                .then((async json => {
+                                    if (json.data) {
+                                        // res.send({ status: 200, message: 'get crop list succeed', json: json })
+                                        let ketemu = null;
+                                        await json.data.map(async (item, index) => {
+                                            if (item.email_supplier === req.body.email) {
+                                                ketemu = await item
+                                            }
+                                        })
+                                        if (ketemu) {
+                                            var token = jwt.sign({}, jsonwebtokensecret);
+                                            res.send({ status: 200, message: 'Selamat, Login Berhasil', token: token })
+                                        } else {
+                                            res.send({ status: 501, message: 'Maaf, Login Gagal, Nama Owner tidak ada di database' })
+                                        }
+                                    } else {
+                                        res.send({ status: 502, message: 'get owner list failed', json: json })
+                                    }
+                                }
+                                ))
+                                .catch((err) => {
+                                    res.send({ status: 503, message: 'get owner list failed', err: err })
+                                })
+                        }).catch((err) => {
+                            res.send({ status: 504, message: 'get owner list failed', err: err })
+                        })
+                    } else {
+                        res.send({ status: 505, message: 'Login Gagal' })
+                    }
+                })
+                .catch((err) => {
+                    res.send({
+                        status: 506,
+                        message: 'Login Gagal'
+                        // err : err 
+                    })
+                })
+        })
+        .catch((err) => {
+            res.send({
+                status: 507,
+                message: 'Login Gagal'
+                // err: err
+            })
+        })
 })
 
 module.exports = router
