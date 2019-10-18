83560a680a914b52c34374536cb7520662dbc2c3
Denis Enrico Hasyim <denisenricohasyim93@gmail.com>
2019-10-18 07:01:55 +0700

    x

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get ('/', function (req, res) {
 
 app.use ('/v0', gatewayv0);
 
-app.listen (3009, () => {
+app.listen (7777, () => {
   console.log ('AXA BOS REZA Listen to PORT 7777');
 });
 
